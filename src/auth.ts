3a9dcb28c2293d7ef0b1b4aa8bd756c75319a975
Daniel Silva <daniel.sousasilva97@outlook.com>
2024-09-02 08:36:54 -0300

    chore: includes id to user user session

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,26 @@
-import NextAuth, { CredentialsSignin } from "next-auth";
+import NextAuth, { CredentialsSignin, DefaultSession } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
 import { z, ZodError } from "zod";
+import "next-auth/jwt";
 import { executeQuery } from "./utils/executeQuery";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    userId: string;
+  }
+}
+
 type User = {
+  id: number;
   name: string;
   email: string;
   password: string;
@@ -15,7 +31,7 @@ const schema = z.object({
   password: z.string().min(1),
 });
 
-const query = "SELECT name, email, password FROM users WHERE email = ?";
+const query = "SELECT `id`, `name`, `email`, `password` FROM `users` WHERE `email` = ?";
 
 export type CodeError = "credentials" | "server";
 
@@ -50,6 +66,7 @@ export const {
           if (!isValid) return null;
 
           return {
+            id: user.id.toString(),
             name: user.name,
             email: user.email,
           };
@@ -69,5 +86,20 @@ export const {
   callbacks: {
     // Logged in users are authenticated, otherwise redirect to login page
     authorized: async ({ auth: authData }) => !!authData,
+    jwt: ({ token, user }) => {
+      if (user && user.id) {
+        // eslint-disable-next-line no-param-reassign
+        token.userId = user.id;
+      }
+
+      return token;
+    },
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.userId,
+      },
+    }),
   },
 });
