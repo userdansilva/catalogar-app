f03f8990b53cb61391f17c3f7c5e437e2cec7cf0
Daniel Silva <daniel.sousasilva97@outlook.com>
2024-09-07 11:39:58 -0300

    chore: add reminders at products

--- src/app/app/products/page.tsx
+++ src/app/app/products/page.tsx
@@ -1,10 +1,17 @@
 import { PageLayout } from "@/components/layout/auth/PageLayout";
 import { ProductsTable } from "./_components/products-table";
 import { CreateProductDialog } from "./_components/create-product-dialog";
+import { Alert } from "@/components/ui/Alert";
 
 export default function ProductsPage() {
   return (
     <PageLayout title="Produtos" actionButton={<CreateProductDialog />}>
+      <Alert
+        title="TODOs"
+        description="Adicionar: regras para deletar produtos, textos dos modais"
+        variant="destructive"
+      />
+
       <ProductsTable />
     </PageLayout>
   );
